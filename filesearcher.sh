124183d2093f40eb760dbb78b6545a32056eaa65
Enoch <110155101+enochdeveloper@users.noreply.github.com>
2023-08-11 00:20:12 +1000

    Update filesearcher.sh

--- filesearcher.sh
+++ filesearcher.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#This script searches for files matching the given file extension in a disk partition and prints out the contents of 
+# This script searches for files matching the given file extension in a disk partition and prints out the contents of 
 
 DISK_NAME=$1
 PARTITION_START=$2
@@ -11,11 +11,14 @@ if [[ "$1" = "" || "$2" = "" || "$3" = "" ]]; then
 	exit 9
 fi
 
+# Recursively search through each directory
 search() {
 	fls -o $PARTITION_START $DISK_NAME $1 | while read -r line
 	do
+ 		# Check for a match
 		if [[ "$line" =~ "$FILE_EXTENSION" ]]; then
 			icat -o $PARTITION_START $DISK_NAME $(awk -F'[ :]' '{print $2}' <<< "$line")
+   		# Else search if a directory
 		elif [[ "$line" =~ "d/d" ]]; then
 			search $(awk -F'[ :]' '{print $2}' <<< $line)
 		fi
